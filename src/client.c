96e97505a812200f57bee7a19b38461aac97de99
Brooks <brooks.kindle@wsu.edu>
2014-03-10 22:13:15 -0700

    second initial commit. just created blank client and server main files

---
+++ src/client.c
@@ -0,0 +1,9 @@
+/*
+   Brooks Kindle
+   brooks.kindle@wsu.edu
+
+   client.c    -   entry point for client-side program
+ */
+
+int main(void) {
+}//end main
---
+++ src/server.c
@@ -0,0 +1,9 @@
+/*
+   Brooks Kindle
+   brooks.kindle@wsu.edu
+
+   server.c    -   entry point for server-side program
+ */
+
+int main(void) {
+}//end main
