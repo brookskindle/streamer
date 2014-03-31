79b3da1fdb93b558371164992cbb0127c85d7a36
Brooks <brooks.kindle@wsu.edu>
2014-03-31 00:17:33 -0700

    music playing should work, but can't test it because of an error

--- src/client.c
+++ src/client.c
@@ -6,10 +6,19 @@
  * @date March 2014
  */
 
-//#include <CSFML/Audio.h>
+#include <SFML/Audio.h>
 #include "client.h"
 
 const char MUSIC_FILE[] = ".tmp.ogg";
 
 int play(const char *const filename) {
+	int success = 0;
+	sfMusic *music = sfMusic_createFromFile(filename);
+
+	if(music) { //success opening file
+		sfMusic_play(music); //creates a new thread to play the music in
+		success = 1;
+	}
+
+	return success;
 }//end play
